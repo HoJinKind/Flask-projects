cd539952cca08eae70027628d3c1a0b356dbafd8
jinkind <jinkind_ho@mymail.sutd.edu.sg>
2019-02-01 23:56:35 +0800

    firestore setup

--- Project_1/firebaseConnect.py
+++ Project_1/firebaseConnect.py
@@ -1,7 +1,17 @@
 
 import pyrebase
+import firebase_admin
+from firebase_admin import credentials,firestore
+
+cred = credentials.Certificate("flasktest-a0a4d-firebase-adminsdk-5dtv7-c2f201dc8a.json")
+deafult_app=firebase_admin.initialize_app(cred)
+dbfs = firestore.client()
 def firebaseCall():
 
+    doc_ref =dbfs.collection(u'class').document(u'YLOJ8PzfUsz0U2gSundW')
+    doc_ref.update({'comp_struc.class.test':True})
+    doc =doc_ref.get()
+    
     config = {
         "apiKey": "AIzaSyCCSwHNk4GoIJjIsE26CxE38-ZikAY5qcc",
         "authDomain": "flasktest-a0a4d.firebaseapp.com",
@@ -12,11 +22,7 @@ def firebaseCall():
     };
     firebase = pyrebase.initialize_app(config)
     auth = firebase.auth()
+    dic = doc.to_dict()
+    print(dic['comp struc']['prof'])#able to get data from firestore
 
-    db = firebase.database()
-    pyre_company = db.child("prof/oka").get()#lel
-    ref ="Oka's class: "
-    for company in pyre_company.each():
-        print(company.val())
-        ref +="  " + str(company.val())
-    return ref
+    return "{}".format(doc.to_dict())
